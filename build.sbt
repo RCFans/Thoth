e89e7d509194fb542786d928e2990a2e4ad22366
Justina Chen <rcfans@163.com>
2015-03-02 16:27:59 +0800

    CSV data read

--- build.sbt
+++ build.sbt
@@ -9,5 +9,6 @@ addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
 libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-core" % "1.2.1",
     "org.apache.spark" %% "spark-streaming" % "1.2.1",
-    "org.apache.spark" %% "spark-graphx" % "1.2.1"
+    "org.apache.spark" %% "spark-graphx" % "1.2.1",
+    "org.apache.spark" %% "spark-sql" % "1.2.1"
 )
--- src/main/scala/FreqApp.scala
+++ src/main/scala/FreqApp.scala
@@ -1,3 +1,5 @@
+import org.apache.spark.sql._
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
@@ -8,14 +10,34 @@ import org.apache.spark.{SparkContext, SparkConf}
 object FreqApp {
 
     def main(args: Array[String]) {
-        val path = "freq_sin_noindex.csv"
+        val path = "src/main/resources/sparse_sin.csv"
         val conf = new SparkConf().setMaster("local").setAppName("Frequent Analysis")
         val sc = new SparkContext(conf)
-        val data = sc.textFile(path, 2).cache()
+        val sqlContext = new SQLContext(sc)
 
-        try {
-            val lines = data.map(line => line.split(","))
-        }
+        val dataFile = sc.textFile(path, 2).cache()
+
+        val lines = dataFile.map(_.split(","))
+
+        val titles = lines.first()
+        val size = titles.length
+
+        val schemaString = titles.mkString(" ")
+        val schema = StructType(schemaString.split(" ").map(fieldName => StructField(fieldName, IntegerType , true)))
+
+        val data = lines.zipWithIndex().filter(_._2 > 0).map(_._1)
+
+
+
+        val rowRDD = data.map(p => Row(p.map(s => if (s.length > 0) s.toFloat.toInt else 0):_*))
+
+        val schemaRDD = sqlContext.applySchema(rowRDD, schema)
+
+        schemaRDD.registerTempTable("service")
+
+        val results = sqlContext.sql("SELECT C102002 FROM service WHERE C102002 > 0 LIMIT 10")
+
+        results.foreach(println)
     }
 
 }
